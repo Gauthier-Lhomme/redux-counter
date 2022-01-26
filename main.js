080325295d4bf6e00ea21e717ea8018976d1afbe
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2022-01-26 17:10:34 +0100

    add store subscribe and explanations

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Actions
 const incrementAction = {
   type: "INCREMENT",
 };
@@ -6,7 +7,9 @@ const decrementAction = {
   type: "DECREMENT",
 };
 
-const counterReducer = (state, action) => {
+//Reducer
+let initialState = 0;
+const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case "INCREMENT":
       return state + 1;
@@ -19,4 +22,21 @@ const counterReducer = (state, action) => {
   }
 };
 
-console.log(counterReducer(13, incrementAction));
+// STORE
+const { createStore } = Redux;
+const store = createStore(counterReducer);
+
+//getState() return the current value
+const render = () => console.log(store.getState());
+
+//subscribe will be triggered every time the state change
+store.subscribe(render);
+
+store.dispatch(incrementAction);
+// 1
+
+store.dispatch(decrementAction);
+// 0
+
+store.dispatch(incrementAction);
+// 1
