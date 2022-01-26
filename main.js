7394786b9463a2896fe628574479806cd3190e23
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2022-01-26 16:44:16 +0100

    create reducer

---
+++ main.js
@@ -0,0 +1,15 @@
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+
+    case "DECREMENT":
+      return state - 1;
+
+    default:
+      return state;
+  }
+};
+
+
+console.log(counterReducer(3, { type: "INCREMENT" }))
